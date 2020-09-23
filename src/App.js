e26db7d3967a73902e935159999631b054710eea
Anita Chan <achan@and.digital>
2020-09-23 11:59:58 +0100

    Removed unnecessary imports and changed profile folder structure

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import SearchBar from "./component/SearchBars/SearchBar";
 import Header from "./component/Header/Header";
-import "./styles/SearchBar.css";
 import Carousel from "./component/Carousel/Carousel";
 import Profile from "./component/Profile/Profile";
 import testData from "./component/Profile/testData";
-import "./component/Profile/profile.css";
 
 // Main document that outputs things to the webpage
 function App() {
--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -1,28 +1,29 @@
 import React from "react";
+import "./profile.css";
 import "./testData";
 
 export default function Profile(props) {
   const profile = props.profile;
   return (
-    <div class="prof_card">
+    <div class='prof_card'>
       <strong style={{ color: "#eb7b28" }}>{profile.start}</strong>
       <img
         src={profile.image}
-        alt=""
+        alt=''
         style={{
           width: "100%",
           height: "225px",
           objectFit: "cover",
-          borderRadius: "25PX 25PX 0PX 0PX"
+          borderRadius: "25PX 25PX 0PX 0PX",
         }}
       />
-      <h1 className="prof_name" style={{ color: "#eb7b28" }}>
+      <h1 className='prof_name' style={{ color: "#eb7b28" }}>
         {profile.Name}
       </h1>
-      <h5 className="prof_role">{profile.Role}</h5>
-      <h5 className="prof_location">{profile.Location}</h5>
-      <p className="prof_date"> Joining Date: {profile.Joining_date}</p>
-      <p className="prof_bio">{profile.Bio}</p>
+      <h5 className='prof_role'>{profile.Role}</h5>
+      <h5 className='prof_location'>{profile.Location}</h5>
+      <p className='prof_date'> Joining Date: {profile.Joining_date}</p>
+      <p className='prof_bio'>{profile.Bio}</p>
       <p>
         <button style={{ backgroundColor: "#0a7496", borderRadius: "25PX" }}>
           Contact
